a27d552adb0130103856434c098c63c8e41e4ea6
Joshua Shen <joshua183@asymmfund.com>
2022-11-17 11:24:03 +0800

    update

--- src/crypto/runner.rs
+++ src/crypto/runner.rs
@@ -24,7 +24,7 @@ pub async fn send_request(){
     };
 
     println!("connection SUCCESS");
-    let channel = String::from("book.BTC_ETH");
+    let channel = String::from("book.ETH_CRO");
     let message = Message::from(subscribe_message(channel));
     
     match stream.send(message).await{
