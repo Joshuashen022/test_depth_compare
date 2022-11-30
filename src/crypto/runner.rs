28b857b8ace63cb8367f500a790faedbe1586d80
Joshua Shen <joshua183@asymmfund.com>
2022-11-30 19:00:12 +0800

    update

--- src/crypto/runner.rs
+++ src/crypto/runner.rs
@@ -4,8 +4,8 @@ use url::Url;
 
 // const TRADE_URL_PC: &str =      "wss://dstream.binance.com/stream?streams=btcusd_221230@trade";
 // const TRADE_URL_PU: &str =      "wss://fstream.binance.com/stream?streams=btcusdt@trade";
-const TRADE_URL_SPOT: &str = "https://api.binance.com";
-// const TRADE_URL_SPOT_1: &str =    "https://api1.binance.com";
+// const TRADE_URL_SPOT: &str = "https://api.binance.com";
+const TRADE_URL_SPOT_1: &str =    "https://api1.binance.com";
 // const API_ORDER_TEST : &str = "/api/v3/order/test";
 const API_ORDER : &str = "/api/v3/order";
 // const API_OPEN_ORDERS : &str = "/api/v3/openOrders";
@@ -14,13 +14,10 @@ pub const SECRET_KEY: &str = "atl3kPizvOkgM366O2OPbotuQpbWIxH2M4IEbvAwwqxey6amjK
 
 pub async fn send_request() {
 
-    let url_str = format!("{}{}", TRADE_URL_SPOT, API_ORDER);
+    let url_str = format!("{}{}", TRADE_URL_SPOT_1, API_ORDER);
     
     let order = BinanceCheckOrder::new();
     let body = order.get_body();
-    
-    println!("url: {}", url_str);
-    println!("body: {}", body);
 
     let res = check_order(url_str, body).await;
 
@@ -31,14 +28,15 @@ pub async fn send_request() {
     println!("Done");
 
 }
-
+// url: https://api.binance.com/api/v3/order
 async fn check_order(url: String, body: String) -> String {
+    let quarry_string = format!("{}?{}", url, body);
+    println!("quarry_string: {}", quarry_string);
     let url = Url::parse(&url).expect("Bad URL");
     let client = reqwest::Client::new();
     client
         .get(url)
         .header("X-MBX-APIKEY", ACCESS_KEY)
-        .body(body)
         .send()
         .await
         .unwrap()
