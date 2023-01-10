17ead917254a39fb0b3dcd2c9818bd8f1d53e9db
Joshua Shen <joshua183@asymmfund.com>
2023-01-10 17:16:59 +0800

    update

--- src/crypto/runner.rs
+++ src/crypto/runner.rs
@@ -10,7 +10,8 @@ use url::Url;
 // const CRYPTO_TRADE_WSS1: &str = "ws://p38.crypto.local:8080/v2/user";
 const CRYPTO_WEBSOCKET_WSS2: &str = "wss://stream.crypto.com/v2/user";
 // const TRADE_URL_SPOT: &str = "https://api.binance.com";
-const BINANCE_SPOT_WEBSOCKET_ENDPOINT: &str = "wss://stream.binance.com:9443/ws/";
+const CRYPTO_TRADE_HTTP: &str = "https://api.crypto.com/v2";
+// const BINANCE_SPOT_WEBSOCKET_ENDPOINT: &str = "wss://stream.binance.com:9443/ws/";
 // const TRADE_URL_SPOT_1: &str =    "https://api1.binance.com";
 // const API_ORDER_TEST : &str = "/api/v3/order/test";
 // const API_ORDER : &str = "/api/v3/order";
@@ -87,7 +88,7 @@ pub async fn send_http_request() {
     req.sign(ACCESS_KEY, SECRET_KEY);
     println!("Crypto request {:?}", req);
 
-    let address = CRYPTO_WEBSOCKET_WSS2.to_string();
+    let address = CRYPTO_TRADE_HTTP.to_string();
     println!("url {:?}", address);
 
     let client = reqwest::Client::new();
